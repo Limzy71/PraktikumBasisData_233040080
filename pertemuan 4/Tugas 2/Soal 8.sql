1db254540dec27c5264f4f3b8868cd899f9a12af
Limzy <145044598+Limzy71@users.noreply.github.com>
2024-10-16 10:01:11 +0700

    Update Soal 8.sql

--- pertemuan 4/Tugas 2/Soal 8.sql
+++ pertemuan 4/Tugas 2/Soal 8.sql
@@ -1,3 +1,3 @@
 SELECT *
 FROM mahasiswa
-WHERE jurusan = 'Teknik Informatika' AND alamat LIKE '%Jl. Sudirman%' AND tahun_masuk 
+WHERE jurusan = 'Teknik Informatika' AND alamat LIKE '%Jl. Sudirman%' AND tahun_masuk BETWEEN '2018' AND '2020'
